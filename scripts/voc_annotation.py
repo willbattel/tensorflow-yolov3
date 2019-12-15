5917ee8761c3d5474a2e027d0a8658cb09c8cdb1
Will Battel <willbattel@gmail.com>
2019-12-15 02:54:28 -0600

    Move voc to its own directory

--- scripts/voc_annotation.py
+++ scripts/voc_annotation.py
@@ -39,8 +39,8 @@ def convert_voc_annotation(data_path, data_type, anno_path, use_difficult_bbox=T
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--data_path", default="/home/yang/test/VOC/")
-    parser.add_argument("--train_annotation", default="./data/dataset/voc_train.txt")
-    parser.add_argument("--test_annotation",  default="./data/dataset/voc_test.txt")
+    parser.add_argument("--train_annotation", default="./data/voc/voc_train.txt")
+    parser.add_argument("--test_annotation",  default="./data/voc/voc_test.txt")
     flags = parser.parse_args()
 
     if os.path.exists(flags.train_annotation):os.remove(flags.train_annotation)
